d65f26e3a1906f7be18ef0e0212b5a057621ac05
Pratham Shetty <shettypratham521@gmail.com>
2026-09-19 23:02:18 +0530

    Create ValidPalindrome.java

---
+++ ValidPalindrome.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Skip non-alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters ignoring case
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String s = scanner.nextLine();
+
+        System.out.println(isPalindrome(s));
+
+        scanner.close();
+    }
+}
